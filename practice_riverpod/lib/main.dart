d74472d32b26cc2f55e7dbb6cc8e95c192a0a011
Hom bahadur Pathak <pathakhom17@gmail.com>
2026-08-03 21:38:02 +0545

    small fix

--- practice_riverpod/lib/main.dart
+++ practice_riverpod/lib/main.dart
@@ -1,6 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:practice_riverpod/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+void main(){
+  runApp(const ProviderScope(child: MyApp()));
+}
 
-//This is the standard Riverpod architecture you'll use repeatedly:
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
+//
+// This is the standard Riverpod architecture you'll use repeatedly:
 //
 // Model → Represents data.
 // Service → Fetches or saves data.
